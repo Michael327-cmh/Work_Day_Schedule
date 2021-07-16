0cb09ca56e44da866f844824eb22458173e64650
Michael Carmody <mr.carmody89@gmail.com>
2021-07-16 10:07:13 -0400

    Added Save button function initial

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,19 @@
+var ListInput = document.getElementById("segment");
+
+
 var Today = moment().format("LLL");
 $("#currentDay").text(Today);
 
-//var three = moment().format("LT");
-//var open = moment([2021]).fromNow(true);
-//$("#3hrsbefore").text(open);
+var three_before = moment.subtract(3, "h");
+$("#threebefore").text(three_before);
+
+function saveToDo() {
+    var ToDoList = ListInput
+
+    localStorage.setItem("ToDoList");
+};
+
+saveButton.addEventlistener("click", function(event) {
+    event.preventDefault();
+    saveToDo();
+})
