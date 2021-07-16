85682640feabf9a378bebc0d735989da7acc0fdd
Michael Carmody <mr.carmody89@gmail.com>
2021-07-16 06:24:11 -0400

    times and inputs on html

--- Assets/script.js
+++ Assets/script.js
@@ -1,2 +1,6 @@
 var Today = moment().format("LLL");
 $("#currentDay").text(Today);
+
+//var three = moment().format("LT");
+//var open = moment([2021]).fromNow(true);
+//$("#3hrsbefore").text(open);
